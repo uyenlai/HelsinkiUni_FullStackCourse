656760583f3a9d4cd174bc2da7dc5bae8ec930fe
UyenLai <uyenlai6@gmail.com>
2022-06-29 02:20:46 +0300

    Exercise1.2

--- part1/part1_a_b/src/App.js
+++ part1/part1_a_b/src/App.js
@@ -6,18 +6,18 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercise}</p>
+  )
+}
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+       <Part part={props.part1} exercise={props.exercises1}/>
+       <Part part={props.part2} exercise={props.exercises2}/>
+       <Part part={props.part3} exercise={props.exercises3}/>
     </div>
   )
 }
@@ -29,6 +29,7 @@ const Total = (props) => {
     </div>
   )
 }
+
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -44,8 +45,7 @@ const App = () => {
       <Header course={course} />
       <Content part1={part1} exercises1={exercises1}
                part2={part2} exercises2={exercises2}
-               part3={part3} exercises3={exercises3}
-       />
+               part3={part3} exercises3={exercises3} />
       <Total  exercises1={exercises1}
               exercises2={exercises2}
               exercises3={exercises3}
