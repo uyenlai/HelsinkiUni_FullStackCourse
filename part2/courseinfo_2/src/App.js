980cf37d9ecb1a590eb133b8987c23c888e0a805
UyenLai <uyenlai6@gmail.com>
2022-07-06 20:00:19 +0300

    part2

---
+++ part2/courseinfo_2/src/App.js
@@ -0,0 +1,44 @@
+//2.1
+const Header = (props) => {
+  return(
+    <div>{props.name}</div>
+  )
+}
+
+const Course = (props) => {
+  return(
+    <div>
+      <Header />
+      <Content />
+        <Part />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App;
