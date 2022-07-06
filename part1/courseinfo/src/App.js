1e87834cb14fb4f865d65cec1ef95307d8d08824
UyenLai <uyenlai6@gmail.com>
2022-07-06 18:01:37 +0300

    courseinfo - step 5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -152,7 +152,7 @@ export default App
 */
 
 //1.4
-
+/*
 const Header = (props) => {
   return (
     <div>
@@ -203,6 +203,71 @@ const Content = (props) => {
 const Total = (props) => {
   var sum = 0
   const listItems = props.parts.map(function(element) {
+    return (
+      sum = sum + element.exercises
+    )
+  })
+  return (
+    <div>
+       <p>Number of exercises: {sum}</p>
+    </div>
+  )
+}
+
+export default App
+*/
+
+//1.5
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  const listItems = props.course.parts.map(function(element) {
+    return (
+      <div>
+        <p>{element.name}: {element.exercises}</p>
+      </div>
+      )
+  })
+  return listItems
+}
+
+const Total = (props) => {
+  var sum = 0
+  const listItems = props.course.parts.map(function(element) {
     return (
       sum = sum + element.exercises
     )
