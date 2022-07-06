0c8b69775d73c6ea95c4f244ea863d3623f0f7dd
UyenLai <uyenlai6@gmail.com>
2022-07-06 12:58:45 +0300

    courseinfo

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,53 +1,42 @@
 import { useState } from 'react'
 
-// Statistics Component
-const Statistics = (props) => {
-  console.log(props);
-  if (props.good === 0 && props.neutral === 0 && props.bad === 0)
-  return (
-    <div>{props.feedback}</div>
-  )
-  if (props.good !== 0 || props.neutral !== 0 || props.bad !== 0) 
-  return (
-    <div>
-      <p>Good: {props.good}</p>
-      <p>Neutral: {props.neutral}</p>
-      <p>Bad: {props.bad}</p>
-      <p>All: {props.all}</p>
-      <p>Average: {props.average}</p>
-      <p>Positive: {props.positive}</p>
-    </div>
-  )
-}
-
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+  const [selected, setSelected] = useState(0)
+
+  function randomNumInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  const handleCLick = () => {
+    setSelected(randomNumInRange(0, 6))
+  }
+
+  const [voted, setVoted] = useState([0,0,0,0,0,0,0])
+
+  const updateVotes = () => {
+    const copy = [...voted]
+    copy[selected] += 1
+    setVoted(copy)
+  }
+
+  const handleVote = () => {
+    setVoted(voted + 1)
+  }
 
   return (
     <div>
-      <h1>Give Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>
-        Good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-        Neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        Bad
-      </button>
-      <h2>Statistics</h2>
-      <Statistics 
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        all={good + neutral + bad}
-        average={(good + neutral + bad) / 3}
-        positive={((good + neutral) / (good + neutral + bad))*100} 
-        feedback="No feedback given"
-      />
+      {anecdotes[selected]}
+      <p>has {voted[selected]} votes</p>
+      <button onClick={updateVotes}>Vote</button>
+      <button onClick={handleCLick}>Next Anecdote</button>
     </div>
   )
 }
