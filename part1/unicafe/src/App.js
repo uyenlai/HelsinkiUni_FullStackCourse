c5502848c0963bfbfcdf851f56096143552f4173
UyenLai <uyenlai6@gmail.com>
2022-07-10 18:03:25 +0300

    exercise 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -111,6 +111,7 @@ const App = () => {
 */ 
 
 //1.9
+/*
 // Statistics Component
 const Statistics = (props) => {
   console.log(typeof props.feedback)
@@ -149,6 +150,59 @@ const App = () => {
       <button onClick={() => setBad(bad + 1)}>
         Bad
       </button>
+      <h2>Statistics</h2>
+      <Statistics 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={good + neutral + bad}
+        average={(good + neutral + bad) / 3}
+        positive={((good + neutral) / (good + neutral + bad))*100} 
+        feedback=""
+      />
+    </div>
+  )
+}
+*/
+
+//1.10
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
+  
+const StatisticLine = (props) => (
+  <p>{props.text} {props.value}</p>
+)
+
+const Statistics = (props) => {
+  console.log(typeof props.feedback)
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0)
+  return (
+    <div><p>No feedback given {props.feedback}</p></div>
+  )
+  if (props.good !== 0 || props.neutral !== 0 || props.bad !== 0)
+  return (
+    <div>
+    <StatisticLine text="Good:" value={props.good} />
+    <StatisticLine text="Neutral:" value={props.neutral} />
+    <StatisticLine text="Bad:" value={props.bad} />
+    <StatisticLine text="All:" value={props.all} />
+    <StatisticLine text="Average:" value={props.average} />
+    <StatisticLine text="Positive:" value={props.positive} />
+    </div>
+  )
+} 
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="Good"/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral"/>
+      <Button handleClick={() => setBad(bad + 1)} text="Bad"/>
       <h2>Statistics</h2>
       <Statistics 
         good={good}
