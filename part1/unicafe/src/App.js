ea444f36553a077d09eb034ee1fde5281a1c8953
UyenLai <uyenlai6@gmail.com>
2022-07-10 15:30:04 +0300

    exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -30,6 +30,7 @@ const App = () => {
 */
 
 //1.7
+/*
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -58,5 +59,52 @@ const App = () => {
     </div>
   )
 }
+*/
 
+//1.8
+// Statistics Component
+const Statistics = (props) => {
+  return (
+    <div>
+      <p>Good: {props.good}</p>
+      <p>Neutral: {props.neutral}</p>
+      <p>Bad: {props.bad}</p>
+      <p>All: {props.all}</p>
+      <p>Average: {props.average}</p>
+      <p>Positive: {props.positive}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>
+        Good
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        Neutral
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        Bad
+      </button>
+      <h2>Statistics</h2>
+      <Statistics 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={good + neutral + bad}
+        average={(good + neutral + bad) / 3}
+        positive={((good + neutral) / (good + neutral + bad))*100} 
+
+      />
+    </div>
+  )
+}
 export default App
