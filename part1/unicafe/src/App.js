ac6cdf047c91ba4ba576de09e16acc27473f8f88
UyenLai <uyenlai6@gmail.com>
2022-07-10 20:50:17 +0300

    exercise 1.12

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -166,6 +166,7 @@ const App = () => {
 */
 
 //1.10
+/*
 const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 )
@@ -173,7 +174,7 @@ const Button = (props) => (
 const StatisticLine = (props) => (
   <p>{props.text} {props.value}</p>
 )
-
+ 
 const Statistics = (props) => {
   console.log(typeof props.feedback)
   if (props.good === 0 && props.neutral === 0 && props.bad === 0)
@@ -216,4 +217,101 @@ const App = () => {
     </div>
   )
 }
+*/ 
+
+//1.11
+/*
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
+  
+const StatisticLine = (props) => (
+  <p>{props.text} {props.value}</p>
+)
+ 
+const Statistics = (props) => {
+  console.log(typeof props.feedback)
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0)
+  return (
+    <div><p>No feedback given {props.feedback}</p></div>
+  )
+  if (props.good !== 0 || props.neutral !== 0 || props.bad !== 0)
+  return (
+    <div>
+      <tr>
+        <td><StatisticLine text="Good:" value={props.good} /></td>
+      </tr>
+      <tr>
+        <td><StatisticLine text="Neutral:" value={props.neutral} /></td>
+      </tr>
+      <tr>
+        <td><StatisticLine text="Bad:" value={props.bad} /></td>
+      </tr>
+      <tr>
+        <td><StatisticLine text="All:" value={props.all} /></td>
+      </tr>
+      <tr>
+        <td> <StatisticLine text="Average:" value={props.average} /></td>
+      </tr>
+      <tr>
+        <td><StatisticLine text="Positive:" value={props.positive} /></td>
+      </tr>
+    </div>
+  )
+} 
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="Good"/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral"/>
+      <Button handleClick={() => setBad(bad + 1)} text="Bad"/>
+      <h2>Statistics</h2>
+      <Statistics 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={good + neutral + bad}
+        average={(good + neutral + bad) / 3}
+        positive={((good + neutral) / (good + neutral + bad))*100} 
+        feedback=""
+      />
+    </div>
+  )
+}
+*/ 
+
+//1.12
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+  const [selected, setSelected] = useState(0)
+
+  function randomNumInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  const handleCLick = () => {
+    setSelected(randomNumInRange(0, 6))
+  };
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <br></br><br></br>
+      <button onClick={handleCLick}>Next Anecdote</button>
+    </div>
+  )
+}
+
 export default App
